4f5c2f1808bd7ae7122b56c1bc18738585bc1793
김지호 <crsn1111@gmail.com>
2023-04-20 23:37:06 +0900

    [feat]/canvas snow

---
+++ snow.js
@@ -0,0 +1,81 @@
+const canvas = document.querySelector('canvas');
+
+const ctx = canvas.getContext('2d')
+const dpr = window.devicePixelRatio
+
+const canvasWidth = innerWidth
+const canvasHeight = innerHeight
+
+canvas.width = canvasWidth * dpr
+canvas.height = canvasHeight * dpr
+
+canvas.style.width = canvasWidth + 'px'
+canvas.style.height = canvasHeight + 'px'
+
+ctx.scale(dpr, dpr)
+
+class Particle {
+  constructor(x, y, radius, vy) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.vy = vy
+    this.acc = 1.01
+  }
+
+  update() {
+    this.vy *= this.acc
+    this.y += this.vy
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI / 180 * 360)
+    ctx.fillStyle = 'white'
+    ctx.fill()
+    ctx.closePath()
+  }
+}
+
+const TOTAL = 500
+let particles = []
+
+const randomNumBetween = (min, max) => {
+  return Math.random() * (max - min + 1) + min
+}
+
+for (let i = 0; i < TOTAL; i++) {
+  const x = randomNumBetween(0, canvasWidth);
+  const y = randomNumBetween(0, canvasHeight)
+  const radius = randomNumBetween(1, 2)
+  const vy = randomNumBetween(1, 5)
+  const particle = new Particle(x, y, radius, vy)
+  particles.push(particle)
+}
+
+let interval = 1000 / 60
+let now, delta
+let then = Date.now()
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  now = Date.now()
+  delta = now - then
+  if (delta < interval) return
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+  particles.forEach(particle => {
+    particle.update()
+    particle.draw()
+
+    if (particle.y - particle.radius > canvasHeight) {
+      particle.y = -particle.radius
+      particle.x = randomNumBetween(0, canvasWidth);
+      particle.radius = randomNumBetween(1, 2)
+      particle.vy = randomNumBetween(1, 2)
+    }
+  })
+  then = now - (delta % interval)
+}
+
+animate()
